9a98d931e330f029c640bc18ea12de4aed8c135d
ogoiddev <diogompantaleao@gmail.com>
2022-09-25 18:42:23 -0300

    chore: logo nodejs

--- development/src/components/Skills/index.tsx
+++ development/src/components/Skills/index.tsx
@@ -73,7 +73,7 @@ export default function Skills() {
           <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nginx/nginx-original.svg" alt="nginx" width="40" height="40" />
         </a>
         <a href="https://nodejs.org" target="_blank" rel="noreferrer">
-          <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40" />
+          <img src="https://nodejs.org/static/images/logo.svg" alt="nodejs" width="40" height="40" />
         </a>
         <a href="https://www.python.org" target="_blank" rel="noreferrer">
           <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40" />
