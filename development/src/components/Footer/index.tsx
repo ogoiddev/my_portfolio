327a47d02d3aaa0b278e9e0736b5f9eed326deed
ogoiddev <diogompantaleao@gmail.com>
2022-10-08 19:12:51 -0300

    merge: resume updated

--- development/src/components/Footer/index.tsx
+++ development/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import { FooterContainer } from "./styles";
 export default function Footer() {
   return (
     <FooterContainer id='footer'>
-      <a className='resume' href="resume_ogoiddev_03-10-2022.pdf" target="_blank" download>
+      <a className='resume' href="./resume_ogoiddev_03-10-2022.pdf" target="_blank" download>
         Estou em busca de oportunidade de trabalho - Clique aqui para ver meu CURRÍCULO</a>
             
     </FooterContainer>
